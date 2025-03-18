489cf8b108ed9d5084ae4253048253a1eeaf2bc9
aymaneids <159579781+aymaneids@users.noreply.github.com>
2025-03-18 15:59:03 +0000

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -67,7 +67,7 @@ const Services = () => {
         <div className="absolute inset-0 -z-10">
           <div className="absolute inset-0 bg-gradient-to-r from-black/40 to-black/20"></div>
           <img
-            src="https://images.unsplash.com/photo-1570172619644-dfd03ed5d881?auto=format&fit=crop&q=80"
+            src="https://mountainsidespa.com/wp-content/uploads/2021/02/Top-7-Factors-to-Consider-When-Choosing-a-Spa.jpg"
             alt="Body sculpting service"
             className="w-full h-full object-cover"
           />
